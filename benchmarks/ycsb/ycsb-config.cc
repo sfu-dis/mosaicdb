25c9f62aad27a58eb7c8c3a4dc2e6039bc81eee9
Tianzheng Wang <tzwang@sfu.ca>
2024-09-26 10:13:22 -0700

    lint.

--- benchmarks/ycsb/ycsb-config.cc
+++ benchmarks/ycsb/ycsb-config.cc
@@ -81,7 +81,7 @@ void ycsb_table_loader::load() {
 
 void ycsb_table_loader::do_load(ermia::OrderedIndex *tbl, std::string table_name, uint64_t hot_record_count, uint64_t cold_record_count) {
   uint32_t nloaders = std::thread::hardware_concurrency() / (numa_max_node() + 1) / 2 * ermia::config::numa_nodes;
-  uint64_t record_per_thread = ((hot_record_count+nloaders-1) / nloaders);
+  uint64_t record_per_thread = ((hot_record_count + nloaders - 1) / nloaders);
 
   uint64_t hot_start_idx = instered_hot_table_size.fetch_add(record_per_thread);
   int64_t hot_to_insert;
@@ -96,12 +96,12 @@ void ycsb_table_loader::do_load(ermia::OrderedIndex *tbl, std::string table_name
   int64_t to_insert = hot_to_insert + cold_to_insert;
 
   uint64_t kBatchSize = 256;
-
   
   ermia::transaction *txn;
-  if(hot_to_insert){
+  if (hot_to_insert) {
     txn = db->NewTransaction(0, *arena, txn_buf()); 
   }
+
   for (uint64_t i = 0; i < hot_to_insert; ++i) {
     ermia::varstr &k = str(sizeof(ycsb_kv::key));
     BuildKey(hot_start_key + i, k);
